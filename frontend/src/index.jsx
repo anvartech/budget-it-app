cf1e18169928f05d95c0b13dd0287ffc20e0130b
Fidelis Ojeah <fidelis_oj@yahoo.co.uk>
2018-10-31 19:42:57 +0000

    use react app eslint

---
+++ frontend/src/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createBrowserHistory } from 'history';
+
+import store from 'utils/store';
+import * as serviceWorker from 'serviceWorker';
+
+import App from './App';
+import './index.css';
+
+const history = createBrowserHistory();
+
+ReactDOM.render(
+    <Router history={history}>
+        <Provider store={store}>
+            <App />
+        </Provider>
+  </Router>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
